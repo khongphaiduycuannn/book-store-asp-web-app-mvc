bbf1e023435da46b0183fc1cec800fb3dfe37ed8
Trần Quốc Doanh <100980663+thisisdoanh@users.noreply.github.com>
2024-06-14 13:39:34 +0700

    save code books

--- BookStoreAdmin/BookStoreAdmin/Models/book.cs
+++ BookStoreAdmin/BookStoreAdmin/Models/book.cs
@@ -1,7 +1,8 @@
-namespace BookStoreAdmin.Models
+﻿namespace BookStoreAdmin.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -17,32 +18,46 @@ namespace BookStoreAdmin.Models
         }
 
         [Key]
+        [DisplayName("Mã sách")]
         public int book_id { get; set; }
 
+        [DisplayName("Mã tác giả")]
         public int? author_id { get; set; }
+        [DisplayName("Mã danh mục")]
 
         public int? category_id { get; set; }
 
         [StringLength(255)]
+        [DisplayName("Tên sách")]
         public string name { get; set; }
 
         [StringLength(255)]
+        [DisplayName("Bìa sách")]
         public string image { get; set; }
+        [DisplayName("Mô tả")]
 
         public string description { get; set; }
 
         [StringLength(255)]
+        [DisplayName("Nhà phát hành")]
         public string publish_company { get; set; }
+        [DisplayName("Năm phát hành")]
 
         public int? publish_year { get; set; }
+        [DisplayName("Giá")]
 
         public int? price { get; set; }
+        [DisplayName("Đã bán")]
 
         public int? sold { get; set; }
+        [DisplayName("Còn lại")]
 
         public int? remain { get; set; }
+        [DisplayName("Trạng thái")]
 
         public int? is_deleted { get; set; }
+        [DisplayName("Ngày tạo")]
+
 
         public DateTime? created_at { get; set; }
 
